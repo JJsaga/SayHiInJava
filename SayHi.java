6b3466064a2fbfe0bb1b782c4cb8eab9782434e1
jj <jjreal615@gmail.com>
2021-10-04 00:23:20 +0000

    hello world

--- SayHi.java
+++ SayHi.java
@@ -1,6 +1,10 @@
 public class SayHi{
 	public static void main(String[] args){
-		System.out.println("Say Hi");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
 		
 		}	
 }
